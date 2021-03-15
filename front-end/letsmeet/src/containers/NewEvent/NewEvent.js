bb96689d4bd44dfd77c0e8263c88705a983a077d
Forzana Rime <fkr206@nyu.edu>
2021-03-15 19:59:40 -0400

    Working on authentication in new event

--- front-end/letsmeet/src/containers/NewEvent/NewEvent.js
+++ front-end/letsmeet/src/containers/NewEvent/NewEvent.js
@@ -4,9 +4,17 @@ import classes from '../NewEvent/NewEvent.module.css'
 import { Form, Input, Button, Divider, DatePicker } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
 import { EnvironmentOutlined } from '@ant-design/icons';
-import ScheduleSelector  from 'react-schedule-selector'
+import ScheduleSelector  from 'react-schedule-selector';
+import { isAuthenticated } from '../../App'
 
 const NewEvent = () => {
+
+    // temporary until authentication is implemented
+    const [state, setState] = useState({
+        isAuthenticated: false
+    });
+
+    // Form
     const [form] = Form.useForm();
     const [requiredMark, setRequiredMarkType] = useState('optional');
 
@@ -21,50 +29,53 @@ const NewEvent = () => {
         setDate(dateString)
         console.log(date, dateString);
     }
-    return (
-        <div className={classes.container}>
-            <Form
-                form={form}
-                layout="vertical"
-                initialValues={{
-                    requiredMark,
-                }}
-                onValuesChange={onRequiredTypeChange}
-                requiredMark={requiredMark}
-            >
-                <Divider orientation="left" className="first">Create New Event</Divider>
-                <Form.Item label="Event Title" required tooltip="This is a required field">
-                    <Input placeholder="ex. birthday party" />
-                </Form.Item>
-                <Form.Item
-                    label="Event Description"
-                    tooltip={{
-                    title: 'Write the event details here',
-                    icon: <InfoCircleOutlined />,
+
+    if (isAuthenticated) {
+        return (
+            <div className={classes.container}>
+                <Form
+                    form={form}
+                    layout="vertical"
+                    initialValues={{
+                        requiredMark,
                     }}
+                    onValuesChange={onRequiredTypeChange}
+                    requiredMark={requiredMark}
                 >
-                    <Input.TextArea autoSize />
-                </Form.Item>
-                <Form.Item name="location" label="Location">
-                    <Input
-                    prefix={<EnvironmentOutlined className="site-form-item-icon" />}
-                    placeholder="Add a place"
+                    <Divider orientation="left" className="first">Create New Event</Divider>
+                    <Form.Item label="Event Title" required tooltip="This is a required field">
+                        <Input placeholder="ex. birthday party" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Event Description"
+                        tooltip={{
+                        title: 'Write the event details here',
+                        icon: <InfoCircleOutlined />,
+                        }}
+                    >
+                        <Input.TextArea autoSize />
+                    </Form.Item>
+                    <Form.Item name="location" label="Location">
+                        <Input
+                        prefix={<EnvironmentOutlined className="site-form-item-icon" />}
+                        placeholder="Add a place"
+                        />
+                    </Form.Item>
+                    <Form.Item name="invite" label="Invite">
+                        <Input placeholder="..." />
+                    </Form.Item>
+                    <Divider orientation="left" className="first">Availability</Divider>
+                    <DatePicker format={dateFormat} onChange={onChange}/><br/>
+                    <ScheduleSelector
+                        hourlyChunks={1}
+                        startDate={startDate}
                     />
-                </Form.Item>
-                <Form.Item name="invite" label="Invite">
-                    <Input placeholder="..." />
-                </Form.Item>
-                <Divider orientation="left" className="first">Availability</Divider>
-                <DatePicker format={dateFormat} onChange={onChange}/><br/>
-                <ScheduleSelector
-                    hourlyChunks={1}
-                    startDate={startDate}
-                />
-                <Form.Item wrapperCol={{ span: 12, offset: 11 }}>
-                    <Button type="primary" className={classes.formButton}>Submit</Button>
-                </Form.Item>
-            </Form>
-        </div>
-    );
+                    <Form.Item wrapperCol={{ span: 12, offset: 11 }}>
+                        <Button type="primary" className={classes.formButton}>Submit</Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        );
+    };
 };
 export default NewEvent;
