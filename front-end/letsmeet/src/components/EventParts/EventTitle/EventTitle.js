e30440810ea11cad86f2d419325d1d51d6b8b969
Bing Chen <bc2486@nyu.edu>
2021-03-15 20:38:27 -0400

    add comments to EventTitle

--- front-end/letsmeet/src/components/EventParts/EventTitle/EventTitle.js
+++ front-end/letsmeet/src/components/EventParts/EventTitle/EventTitle.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 import classes from './EventTitle.module.css';
 
+/*
+    This component displays the title of an event page and its associated pages.
+    The title includes the name, location, and time of the event.
+
+    Props:
+        - title: name of the event
+        - day: day of the week 
+        - date: Month and day of the year
+        - time: time range from start to end
+*/
 const eventTitle = (props) => (
     <div>
         <h1>{props.title}</h1>
