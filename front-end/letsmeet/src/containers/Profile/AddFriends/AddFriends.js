95a29c7ba9b3136a493132c4b36acd2c2b507abb
Crystal Lee <crys@nyu.edu>
2021-03-20 20:44:35 -0400

    Re-organized logic for how users are displayed when searched w/ Forzana

--- front-end/letsmeet/src/containers/Profile/AddFriends/AddFriends.js
+++ front-end/letsmeet/src/containers/Profile/AddFriends/AddFriends.js
@@ -1,15 +1,34 @@
 import React, { useState } from 'react';
 import classes from './AddFriends.module.css';
 import 'antd/dist/antd.css';
-import { Alert, Button} from 'antd';
+import { Alert, Button, Input, Table, Space } from 'antd';
+import avi from '../../../assets/Avatars/redavi.png';
 
 const AddFriends = (props) => {
 
     const dataList = [
-        {id: 1, name: "Alexa Taylor", email: "alexa@gmail.com", friend: "false"},
-        {id: 2, name: "Timothy Sanders", email: "timothy@gmail.com", friend: "true"},
-        {id: 3, name: "Sam Peter", email: "sam@gmail.com", friend: "false"},
-        {id: 4, name: "Matthew Fishman", email: "matt@gmail.com", friend: "false"},
+        {
+            "id": 1, 
+            "name": "Alexa Taylor", 
+            "email": "alexa@gmail.com", 
+            "avatar": {avi}, 
+            "friends": [
+                {
+                "id": 2,
+                "name": "Timothy Sanders"
+                }
+            ]
+        },
+        {"id": 2, "name": "Timothy Sanders", "email": "timothy@gmail.com",
+            "friends": [
+                {
+                "id": 1,
+                "name": "Alexa Taylor"
+                }
+            ]
+        },
+        {"id": 3, "name": "Sam Peter", "email": "sam@gmail.com", "friend": "false"},
+        {"id": 4, "name": "Matthew Fishman", "email": "matt@gmail.com", "friend": "false"},
         {id: 5, name: "Matthew Fishman", email: "matt2@gmail.com", friend: "false"},
         {id: 6, name: "Matthew Fishman", email: "matt3@gmail.com", friend: "false"},
         {id: 7, name: "Matt Fish", email: "mattf@gmail.com", friend: "false"},
@@ -18,28 +37,67 @@ const AddFriends = (props) => {
         {id: 10, name: "Matt Fishman", email: "fishmanmatt@gmail.com", friend: "true"}
     ]
 
+    const { Search } = Input;
+
     const [searchTerm, setSearchTerm] = useState("")
-    const [data, setData] = useState(dataList)
+    const [data, setData] = useState(dataList) // change to props.friends
 
     // Exclude these cols when searching term
     const excludeColumns = ["id", "name"];
 
+    const columns = [
+        {
+          dataIndex: 'name',
+          key: 'name',
+          render: text => <a>{text}</a>,
+        },
+        {
+          key: 'action',
+          render: (text, record) => (
+            <Space size="middle">
+              <a>Add</a>
+              <a>Delete</a>
+            </Space>
+          ),
+        },
+      ];
+      
     const handleChange = e => {
         setSearchTerm(e)
-        filterData(e)
+        validateFriend(e)
     }
 
-    const filterData = (e) => {
+    const me = [
+        { 
+            "id": 11, 
+            "name": "Alexa Taylor", 
+            "email": "me@gmail.com", 
+            "avatar": {avi}, 
+            "friends": [
+                {
+                    "id": 2,
+                    "name": "Timothy Sanders"
+                },
+                {
+                    "id": 3,
+                    "name": "Sam Peters"
+                }
+            ]
+        }
+    ]
+
+    const validateFriend = (e) => {
         const lowercaseSearch = e.toLowerCase().trim()
         if(lowercaseSearch === "") {
-            setData(dataList)
+            setData()
         } else {
             const filteredData = dataList.filter(item => {
-                return Object.keys(item).some(key =>
-                    excludeColumns.includes(key) ? false : item[key].toString().toLowerCase().includes(lowercaseSearch)
+                return Object.keys(item).some(e =>
+                    excludeColumns.includes(e) ? false : item[e].toString().toLowerCase().includes(lowercaseSearch)
                 )
             })
             setData(filteredData)
+            console.log(filteredData)
         }
     }
     
@@ -47,28 +105,30 @@ const AddFriends = (props) => {
         <div className={classes.container}>
             <h1>Add friends</h1>
             <p>Search for a friend by email address</p>
-            <input
+            <Search
                 placeholder="Add user by email"
-                value={searchTerm}
-                onChange={e => handleChange(e.target.value)}
+                onSearch={handleChange}
+                enterButton
             />
-            <div>
-                {data.map((d, i) => {
-                    return <div key={i} className={classes.nameDisplay}>
-                        {d.name}<br/> <Button size="small" type="primary" className={classes.button}>add</Button>
-                        <div>{d.email}</div>
-                        <br/>
-                    </div>
-                })}
-                <div className="clearboth"></div>
-                {data.length === 0 && 
-                    <span> 
-                        No user with the email provided was found. <br/> 
-                        Would you like to invite a friend? <br/> 
-                        <button>Invite</button>
-                    </span>
-                }
-            </div>
+            {searchTerm && 
+                <div>
+                    {data.map((d, i) => {
+                        return <div key={i} className={classes.nameDisplay}>
+                            {d.name}<br/> <Button size="small" type="primary" className={classes.button}>add</Button>
+                            <div>{d.email}</div>
+                            <br/>
+                        </div>
+                    })}
+                    <div className="clearboth"></div>
+                    {data.length === 0 && 
+                        <span> 
+                            No user with the email provided was found. <br/> 
+                            Would you like to invite a friend? <br/> 
+                            <button>Invite</button>
+                        </span>
+                    }
+                </div>
+            }
         </div>
     )
 }
