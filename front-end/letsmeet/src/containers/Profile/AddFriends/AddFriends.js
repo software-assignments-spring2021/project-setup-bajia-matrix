2bcae6ec18e54725107bfdcbbcbbfe964436ff0a
Crystal Lee <crys@nyu.edu>
2021-03-21 19:26:23 -0400

    add button only displays for non-friends

--- front-end/letsmeet/src/containers/Profile/AddFriends/AddFriends.js
+++ front-end/letsmeet/src/containers/Profile/AddFriends/AddFriends.js
@@ -23,74 +23,42 @@ const AddFriends = (props) => {
         {"id": 2, "name": "Timothy Sanders", "email": "timothy@gmail.com",
             "friends": [
                 {
-                "id": 1,
-                "name": "Alexa Taylor",
-                "email": "alexa@gmail.com"
+                    "id": 1,
+                    "name": "Alexa Taylor",
+                    "email": "alexa@gmail.com"
+                },
+                {
+                    "id": 11,
+                    "name": "Test", 
+                    "email": "me@gmail.com", 
                 }
             ]
         },
-        {"id": 3, "name": "Sam Peter", "email": "sam@gmail.com", "friend": "false"},
-        {"id": 4, "name": "Matthew Fishman", "email": "matt@gmail.com", "friend": "false"},
-        {id: 5, name: "Matthew Fishman", email: "matt2@gmail.com", friend: "false"},
-        {id: 6, name: "Matthew Fishman", email: "matt3@gmail.com", friend: "false"},
-        {id: 7, name: "Matt Fish", email: "mattf@gmail.com", friend: "false"},
-        {id: 8, name: "Matt Fishman", email: "fishmanm@gmail.com", friend: "false"},
-        {id: 9, name: "Matt Fishman", email: "fishman1@gmail.com", friend: "true"},
-        {id: 10, name: "Matt Fishman", email: "fishmanmatt@gmail.com", friend: "true"}
+        {
+            "id": 3, 
+            "name": "Sam Peter", 
+            "email": "sam@gmail.com", 
+            "friend": [
+                {
+                    "id": 11,
+                    "name": "Test", 
+                    "email": "me@gmail.com", 
+                }
+            ]
+        },
+        {"id": 4, "name": "Matthew Fishman", "email": "matt@gmail.com"},
+        {id: 5, name: "Matthew Fishman", email: "matt2@gmail.com"},
+        {id: 6, name: "Matthew Fishman", email: "matt3@gmail.com"},
+        {id: 7, name: "Matt Fish", email: "mattf@gmail.com"},
+        {id: 8, name: "Matt Fishman", email: "fishmanm@gmail.com"},
+        {id: 9, name: "Matt Fishman", email: "fishman1@gmail.com"},
+        {id: 10, name: "Matt Fishman", email: "fishmanmatt@gmail.com"}
     ]
 
     const { Search } = Input;
 
     const [searchTerm, setSearchTerm] = useState("")
     const [data, setData] = useState(dataList) // change to props.users
-
-    // Exclude these cols when searching term
-    const excludeColumns = ["id", "name"];
-
-    const columns = [
-        {
-          dataIndex: 'name',
-          key: 'name',
-          render: text => <a>{text}</a>,
-        },
-        {
-          key: 'action',
-          render: (text, record) => (
-            <Space size="middle">
-              <a>Add</a>
-              <a>Delete</a>
-            </Space>
-          ),
-        },
-    ];
-      
-    const handleChange = e => {
-        setSearchTerm(e)
-        validateFriend(e)
-        checkFriendship()
-    }
-
-    const [error, setError] = useState()
-
-    const validateFriend = (e) => {
-        const lowercaseSearch = e.toLowerCase().trim()
-        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-        if (!pattern.test(e)) {
-            setError("Please enter valid email address")
-        } else {
-            setError()
-        }
-        if(lowercaseSearch === "") {
-            setData()
-        } else {
-            const filteredData = dataList.filter(item => {
-                return Object.keys(item).some(e =>
-                    excludeColumns.includes(e) ? false : item[e].toString().toLowerCase().includes(lowercaseSearch)
-                )
-            })
-            setData(filteredData)
-        }
-    }
 
     const me = [
         { 
@@ -112,19 +80,48 @@ const AddFriends = (props) => {
             ]
         }
     ]
-    let isFriend = false;
-    function checkFriendship() {
-        // if the search term is already a friend's email don't display add button: return false
-        console.log(searchTerm)
-        for (var friend in me[0].friends) {
-            console.log(friend.email)           
+
+      
+    const handleChange = e => {
+        setSearchTerm(e)
+        validateFriend(e)
+        checkFriendship(e)
+    }
+
+    const [error, setError] = useState()
+
+    // Exclude these cols when searching term
+    const excludeColumns = ["id", "name"];
+
+    const validateFriend = (e) => {
+        const lowercaseSearch = e.toLowerCase().trim()
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        if (!pattern.test(e)) {
+            setError("Please enter valid email address")
+        } else {
+            setError()
         }
-        me[0].friends.filter(item => {
-            return Object.keys(item).some(searchTerm =>
-                excludeColumns.includes(searchTerm) ? isFriend = true : isFriend = false
-            )
-        })
-        console.log(isFriend)
+        if(lowercaseSearch === "") {
+            setData()
+        } else {
+            const filteredData = dataList.filter(item => {
+                return Object.keys(item).some(e =>
+                    excludeColumns.includes(e) ? false : item[e].toString().toLowerCase().includes(lowercaseSearch)
+                )
+            })
+            setData(filteredData)
+        }
+    }
+
+    const [isFriend, setIsFriend] = useState(false)
+
+    function checkFriendship(e) {
+        for (var friend of me[0].friends) {
+            if(friend.email == e) {
+                setIsFriend(true)
+                break
+            }
+        }
     }
 
     return (        
@@ -140,13 +137,12 @@ const AddFriends = (props) => {
             {(searchTerm && error) &&
                 <p className={classes.errorMessage}>Enter a valid email address!</p>
             }
-
             {(searchTerm && !error) &&
                 <div>
                     {data.map((d, i) => {
                         return (
                             <div key={i} className={classes.nameDisplay}>
-                                {d.name} <br/> 
+                                {d.name} <br/>
                                 {!isFriend && 
                                     <Button size="small" type="primary" className={classes.button}>add</Button>
                                 }
