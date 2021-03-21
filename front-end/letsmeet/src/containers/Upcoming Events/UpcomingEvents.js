63b02ee5a5634fb1419a19553e4af18b23ca9619
rahulbdas1 <rbd285@nyu.edu>
2021-03-20 23:57:20 -0500

    edit supplies and description

--- front-end/letsmeet/src/containers/Upcoming Events/UpcomingEvents.js
+++
@@ -1,4 +0,0 @@
-import description from '../../components/EventParts/EventDescription/EventDescription';
-import suppplies from '../../components/EventParts/EventSupplies/EventSupplies';
-import React, { useEffect, useState } from 'react';
-
