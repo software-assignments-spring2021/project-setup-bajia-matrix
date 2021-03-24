0338a3d02cf7496dd9f2e351de54d0e2c100a199
Bing Chen <bc2486@nyu.edu>
2021-03-24 17:07:02 -0400

    implemented backend for home page

--- back-end/letsmeet/routes/home/home.js
+++ back-end/letsmeet/routes/home/home.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
-require("dotenv").config({ silent: true })
+require("dotenv").config({ silent: true }); // save private data in .env file
 
 router.get("/events", (req, res, next) => {
-    console.log("get request on route /events")
-    res.send("hello")
-    // axios.get(`${process.env.API_BASE_URL}/events.json?key=${process.env.API_SECRET_KEY}`)
-    // axios.get("https://my.api.mockaroo.com/events.json?key=fe6891f0")
-    //     .then(response => {
-    //         res.json(response.data);
-    //     })
-    //     .catch(error => {
-    //         next(error);
-    //     });
+    console.log("get request on route /events");
+    
+    axios.get(`${process.env.API_BASE_URL}/events.json?key=${process.env.API_SECRET_KEY}`)
+        .then(response => {
+            // console.log(response.data);
+            res.json(response.data);
+        })
+        .catch(error => {
+            next(error);
+        });
+});
+
+router.get("/invites", (req, res, next) => {
+    console.log("get request on route /invites");
+
+    axios.get(`${process.env.API_BASE_URL}/invites.json?key=${process.env.API_SECRET_KEY}`)
+        .then(response => {
+            res.json(response.data);
+        })
+        .catch(error => {
+            next(error);
+        });
 });
 
 module.exports = router;
