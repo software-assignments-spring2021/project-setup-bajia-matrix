0424788a7b856d0adfcd624584c23df4929bc7c8
Bing Chen <55264286+bingychen@users.noreply.github.com>
2021-04-30 15:12:10 -0400

    Update splitCosts.test.js

--- back-end/letsmeet/test/splitCosts.test.js
+++ back-end/letsmeet/test/splitCosts.test.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const expect = require("chai").expect;
-const request = require("supertest");
 const axios = require("axios");
-
 
 // create new app separate to avoid testing on the server app
 const app = express();
 app.use("/splitCosts", require("../routes/events/splitCosts"));
 
 describe("SplitCosts routes", () => {
-    describe("Post request to database", () => {
+    describe("Calulate the average cost of supplies and subtract it from each person", () => {
         it("should put a list of supplies through an algorithm and get the cost each person owes", async () => {
             axios.post("/splitCosts")
                 .then(response => {
