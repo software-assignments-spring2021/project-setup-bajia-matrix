24d910632228629b23f1e4e005588b98b3ffca89
Forzana Rime <fkr206@nyu.edu>
2021-04-30 17:38:21 -0400

    deployment edit files

--- front-end/letsmeet/src/axios.js
+++ front-end/letsmeet/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.API_BASE_URL
+    baseURL: process.env.REACT_APP_BASE_URL
     // baseURL: "http://localhost:4000"
 });
 
